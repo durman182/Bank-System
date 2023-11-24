112c909566c25b3a0e81aeab6878213d1056e022
durman182 <74454895+durman182@users.noreply.github.com>
2023-11-24 14:33:48 +0100

    ClassRun.java

--- src/ClassRun.java
+++ src/ClassRun.java
@@ -1,17 +1,5 @@
 import java.awt.EventQueue;
 
-/*
- *   Práca ktorú ponúkate ma napĺňa.
- *   Rád čelím výzvam.
- *   Chcem sa učiť a rásť v odbore. 
- *   Podieľať sa na vývoji nových aj existujúcich programov.
- *   Práve pracujem na projekte "Bankový systém". 
- *   Pre klienta banky program komunikuje cez Rest Api ( prihlásenie,  prijímanie a odosielanie dát). 
- *   Pre zamestnanca  banky program komunikuje cez SQL ( prihlásenie,  prijímanie a odosielanie dát). 
- *   Ako klient banky môže vykonávať prevody medzi účtami cez formulár alebo QR kódom, prijímať a posielať správy, zobrať si úver, atď..... 
- *   A ako zamestnanec podľa pozície, admin má prístup na spravovanie celej aplikácie, manažér spravuje len určenú časť ( pozíciu ), napríklad schvaľovať úvery klientov,  vytvárať nové ponuky k kartám ( kreditným, debetným ,atď. ), úverom ( bežný úver , hypotéka) alebo sporiacim účtom(terminovaný vklad, investičné sporenie).   
- */
-
 import UIDesign.MainWindow;
 
 public class ClassRun {
